f402ab2ece3418ff46c2d4724cdc180e2112ae91
Ankur Sinha <14087196+sinhaankur@users.noreply.github.com>
2022-08-05 21:32:40 -0400

    Updated

---
+++ js/googletag.js
@@ -0,0 +1,10 @@
+//Googletag
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BHWHS9S8Q8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BHWHS9S8Q8');
+</script>
