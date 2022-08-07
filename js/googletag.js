0ee1b6379909bb856799fad16609947ca737d503
Ankur Sinha <14087196+sinhaankur@users.noreply.github.com>
2022-08-07 12:23:21 -0400

    update sunday

--- js/googletag.js
+++ js/googletag.js
@@ -1,10 +1,9 @@
 //Googletag
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BHWHS9S8Q8"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <><script async src="https://www.googletagmanager.com/gtag/js?id=G-BHWHS9S8Q8"></script><script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)};}
+    gtag('js', new Date());
 
-  gtag('config', 'G-BHWHS9S8Q8');
-</script>
+    gtag('config', 'G-BHWHS9S8Q8');
+  </script></>
